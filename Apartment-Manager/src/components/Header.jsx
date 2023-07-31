2a875e212ef5ed4b591791547ce03946e2f31696
Ali Amin <aliamin588@gmail.com>
2023-08-01 00:11:44 +0500

    apartment detail page complete

--- Apartment-Manager/src/components/Header.jsx
+++ Apartment-Manager/src/components/Header.jsx
@@ -19,11 +19,11 @@ export const Header = () => {
             <div className="items-center col-span-6">
               <div className="flex items-center px-6 py-4 max-w-md">
                 <div className="w-full">
-                  <label for="search" className="sr-only">Search</label>
+                  <label htmlFor="search" className="sr-only">Search</label>
                   <div className="relative">
                     <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                       <svg className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
+                        <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clipRule="evenodd" />
                       </svg>
                     </div>
                     <input id="search" name="search" className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Search" type="search" />
@@ -35,20 +35,20 @@ export const Header = () => {
               <button type="button" className="-mx-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
                 <span className="sr-only">Open menu</span>
 
-                <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
 
-                <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
               </button>
             </div>
             <div className="items-center col-span-4 flex ml-auto">
               <Link to="#" className="ml-5 flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                 <span className="sr-only">View notifications</span>
-                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                 </svg>
               </Link>
 
@@ -95,8 +95,8 @@ export const Header = () => {
               </div>
               <button type="button" className="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                 <span className="sr-only">View notifications</span>
-                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                 </svg>
               </button>
             </div>
--- Apartment-Manager/src/pages/apartmentDetail/apartmentDetail.jsx
+++ Apartment-Manager/src/pages/apartmentDetail/apartmentDetail.jsx
@@ -1,82 +1,158 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { HOC } from '../../components/HOC';
-import HouseIcon from '@mui/icons-material/House';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import BedIcon from '@mui/icons-material/Bed';
-import BathtubIcon from '@mui/icons-material/Bathtub';
-import HouseSidingIcon from '@mui/icons-material/HouseSiding';
-import mapboxgl from 'mapbox-gl';
+import React, { useEffect, useRef, useState } from "react";
+import { HOC } from "../../components/HOC";
+import HouseIcon from "@mui/icons-material/House";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import BedIcon from "@mui/icons-material/Bed";
+import BathtubIcon from "@mui/icons-material/Bathtub";
+import HouseSidingIcon from "@mui/icons-material/HouseSiding";
+import mapboxgl from "mapbox-gl";
+import SquareFootIcon from "@mui/icons-material/SquareFoot";
+import MonitorHeartIcon from "@mui/icons-material/MonitorHeart";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import GarageIcon from "@mui/icons-material/Garage";
 
 const mapBoxKey = process.env.MAPBOX_API_KEY;
-mapboxgl.accessToken = "pk.eyJ1IjoiZmFyaGFuMTM0IiwiYSI6ImNsa3I4Ymg3ejA4YjIzZWxqZGVjMnVpNmwifQ.n9lScoAtvLdBsN1AUH8PwA"
-console.log(mapBoxKey);
+mapboxgl.accessToken =
+  "";
 
 const ApartmentDetail = () => {
-    const imageList = [
+  const imageList = [];
 
-    ]
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(4);
 
-
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
-
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [lng, lat],
-            zoom: zoom
-        });
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom,
     });
+  });
 
-    return (
-        <HOC>
-            <div className='grid grid-cols-12 gap-5 justify-evenly'>
-                <div className='col-span-5 rounded-lg'>
-                    <div>
-                        <div>
-                            <img className='w-full h-3/4 rounded-lg' src={require("../../assets/images/apartImage.jpg")} alt='Apartment' />
-                        </div>
-                        <div className='flex flex-row overflow-x-scroll my-2 gap-4'>
-                            <div>
-                                <img className='w-[160px] h-full rounded' src={require("../../assets/images/apartImage.jpg")} alt='Apartment' />
-                            </div>
-                            <div>
-                                <img className='w-[160px] h-full rounded' src={require("../../assets/images/apartImage.jpg")} alt='Apartment' />
-                            </div>
-                            <div>
-                                <img className='w-[160px] h-full rounded' src={require("../../assets/images/apartImage.jpg")} alt='Apartment' />
-                            </div>
-                            <div>
-                                <img className='w-[160px] h-full rounded' src={require("../../assets/images/apartImage.jpg")} alt='Apartment' />
-                            </div>
-                            <div>
-                                <img className='w-[160px] h-full rounded' src={require("../../assets/images/apartImage.jpg")} alt='Apartment' />
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div className="h-[400] relative mt-10 rounded">
-                        <div ref={mapContainer} />
-                    </div>
-                </div>
-                <div className='col-span-7 rounded-lg bg-slate-200'>
-                    <p>Some description in the house for some result</p>
-                    <p> <HouseIcon /> <span className='ml-2'>XYZ street central block, New York, USA</span> </p>
-                    <p> <AttachMoneyIcon /> <span>$300,000</span> </p>
-                    <div className='grid grid-cols-12 gap-5 justify-between'>
-                        <p className='col-span-4'> <BedIcon /> <span>2</span></p>
-                        <p className='col-span-4'> <BathtubIcon /> <span>2</span></p>
-                        <p className='col-span-4'> <HouseSidingIcon /> <span>2</span></p>
-                    </div>
-                </div>
+  return (
+    <HOC>
+      <div className="grid grid-cols-12 gap-5 justify-evenly">
+        <div className="col-span-5 rounded-lg">
+          <div>
+            <div>
+              <img
+                className="w-full h-3/4 rounded-lg"
+                src={require("../../assets/images/apartImage.jpg")}
+                alt="Apartment"
+              />
             </div>
-        </HOC>
-    )
-}
-export default ApartmentDetail
+            <div className="grid grid-cols-2 gap-4 mt-2 h-[50vh] overflow-auto">
+              <div>
+                <img
+                  className="w-full h-full rounded"
+                  src={require("../../assets/images/apartImage.jpg")}
+                  alt="Apartment"
+                />
+              </div>
+              <div>
+                <img
+                  className="w-full h-full rounded"
+                  src={require("../../assets/images/apartImage.jpg")}
+                  alt="Apartment"
+                />
+              </div>
+              <div>
+                <img
+                  className="w-full h-full rounded"
+                  src={require("../../assets/images/apartImage.jpg")}
+                  alt="Apartment"
+                />
+              </div>
+              <div>
+                <img
+                  className="w-full h-full rounded"
+                  src={require("../../assets/images/apartImage.jpg")}
+                  alt="Apartment"
+                />
+              </div>
+              <div>
+                <img
+                  className="w-full h-full rounded"
+                  src={require("../../assets/images/apartImage.jpg")}
+                  alt="Apartment"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-7 ">
+          <div className="rounded-lg bg-white border p-5">
+            <p>Some description in the house for some result</p>
+            <p className="mt-4">
+              {" "}
+              <HouseIcon />{" "}
+              <span className="ml-2">
+                XYZ street central block, New York, USA
+              </span>{" "}
+            </p>
+            <p className="mt-4 font-extrabold text-lg">
+              {" "}
+              <AttachMoneyIcon /> <span>$300,000</span>{" "}
+            </p>
+            <div className="grid grid-cols-12 gap-5 justify-between mt-4">
+              <p className="col-span-4">
+                {" "}
+                <BedIcon /> <span>2</span>
+              </p>
+              <p className="col-span-4">
+                {" "}
+                <BathtubIcon /> <span>2</span>
+              </p>
+              <p className="col-span-4">
+                {" "}
+                <HouseSidingIcon /> <span>2</span>
+              </p>
+            </div>
+            <div className="grid grid-cols-12 gap-5 justify-between mt-4">
+              <p className="col-span-4">
+                {" "}
+                <SquareFootIcon /> <span>2</span>
+              </p>
+              <p className="col-span-4">
+                {" "}
+                <MonitorHeartIcon /> <span>2</span>
+              </p>
+              <p className="col-span-4">
+                {" "}
+                <CalendarMonthIcon /> <span>2</span>
+              </p>
+            </div>
+            <div className="grid grid-cols-12 gap-5 justify-between mt-4">
+              <p className="col-span-4">
+                {" "}
+                <LocationCityIcon /> <span>Single Family</span>
+              </p>
+              <p className="col-span-4">
+                {" "}
+                <GarageIcon /> <span>2</span>
+              </p>
+            </div>
+            <p className="mt-4">Listed By: ome person</p>
+            <p className="mt-4">Last Checked: Date</p>
+
+            <div className="w-fit ml-auto">
+                <button className="bg-sky-600 p-4 rounded-lg text-white hover:bg-sky-500">Contact Us</button>
+            </div>
+          </div>
+
+          <div className="border bg-white relative mt-10 rounded-lg p-4">
+            <div ref={mapContainer} className="map-container h-[500px]" />
+          </div>
+
+        </div>
+      </div>
+    </HOC>
+  );
+};
+export default ApartmentDetail;
